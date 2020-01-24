a5030ba0a830ac78f1f54ce3dc568f647e343d00
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2020-01-24 23:11:01 +0700

    first commit

--- liff-starter.js
+++ liff-starter.js
@@ -3,7 +3,7 @@ window.onload = function() {
   const defaultLiffId = ""; // change the default LIFF value if you are not using a node server
 
   // DO NOT CHANGE THIS
-  let myLiffId = "";
+  let myLiffId = "line://app/1653717029-3WbdeWmN";
 
   // if node is used, fetch the environment variable and pass it to the LIFF method
   // otherwise, pass defaultLiffId
